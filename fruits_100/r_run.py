fc051d44c785fd75dde97e75503941b7181defb3
Ramin Zarebidoky <Ramin.zarebidoky@gmail.com>
2024-05-04 15:51:26 +0330

    Run the program with the new r_utils

--- fruits_100/r_run.py
+++ fruits_100/r_run.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn
 
-from r_utils import prepare_and_get_data_path, find_device, load_model, load_data, train, evaluate, predict
+from r_utils import prepare_and_get_data_path, find_device, load_model, load_train_data, load_valid_data, train, \
+    evaluate, predict
 
 from matplotlib import pyplot as plt
 
@@ -18,19 +19,21 @@ def main():
 
     data_path = prepare_and_get_data_path('pc')
     #
-    # model = load_model(device)
-    # model = model.to(device)
+    model = load_model(device)
+    model = model.to(device)
     #
     # train_data_loader, val_data_loader = load_data(data_path)
+    train_data_loader = load_train_data(data_path / 'train')
+    valid_data_loader = load_valid_data(data_path / 'val')
     #
-    # loss_fn = nn.CrossEntropyLoss()
-    # optimizer = torch.optim.Adam(model.parameters())
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters())
     #
-    # for epoch in range(5):
-    #     print(f'in epoch: {epoch}')
-    #     train(model, train_data_loader, loss_fn, optimizer, device)
-    #     accuracy, loss = evaluate(model, val_data_loader, loss_fn, device)
-    #     print(f'validation -> accuracy: {accuracy:.2f}, loss: {loss:.2f}')
+    for epoch in range(5):
+        print(f'in epoch: {epoch}')
+        train(model, train_data_loader, loss_fn, optimizer, device)
+        accuracy, loss = evaluate(model, valid_data_loader, loss_fn, device)
+        print(f'validation -> accuracy: {accuracy:.2f}, loss: {loss:.2f}')
 
     # haha = Path('/home/ramin/ramin_programs/files/datasets/fruits-100/test/0/0.jpg')
     #
@@ -43,15 +46,15 @@ def main():
     # kekw = tr(image).to(device)
     # kekw = kekw.unsqueeze(0)
     #
-    model = load_model(device)
+    # model = load_model(device)
     #
     # result = model(kekw)
     #
     # print(torch.argmax(result))
 
-    result = predict(model, data_path / 'test')
+    # result = predict(model, data_path / 'test')
 
-    print(result)
+    # print(result)
 
 
 if __name__ == '__main__':
